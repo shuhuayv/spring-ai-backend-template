fa7f829a274b84b692e77eb37a859d78ccd8da11
shuhuayv <3309365091@qq.com>
2026-07-05 12:51:34 +0800

    feat: initialize Spring Boot backend template

---
+++ src/main/java/com/shuhuayv/template/SpringAiBackendTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.shuhuayv.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiBackendTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiBackendTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shuhuayv/template/SpringAiBackendTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuhuayv.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiBackendTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
